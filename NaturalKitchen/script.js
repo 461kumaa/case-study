8d7d48794b1978e0b81390c20c79ffb5e40dc77f
461kumaa <comnom55@gmail.com>
2025-03-27 00:18:58 +0900

    3月ケーススタディ（3/26更新）をアップロード

--- NaturalKitchen/script.js
+++ NaturalKitchen/script.js
@@ -1,8 +1,27 @@
+// カルーセル
 $('.carousel-fade').slick({
   autoplay: true,
   dots: true,
+  arrows: false,
   infinite: true,
   autoplayspeed: 500,
   fade: true,
   cssEase: 'linear'
+});
+
+// スクロール時セクションをフェードイン
+$(window).scroll(function () {
+  const scrollAmount = $(window).scrollTop();
+  const windowHeight = $(window).height();
+  $('section').each(function () {
+    const position =$(this).offset().top;
+    if (scrollAmount > position - windowHeight + 100) {
+      $(this).addClass('fade-in');
+    }
+  });
+});
+
+// ヘッダー追従
+$(window).on('scroll', function() {
+  $('header').toggleClass('fixed', $(this).scrollTop() > 500);
 });
